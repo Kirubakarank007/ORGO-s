b90369f8fe1a72516505f1f4c6defcc35c3e4acf
KIRUBAKARAN K <kirubakaran02002@gmail.com>
2024-10-15 21:13:18 +0530

    Popular Products component added in shop page

---
+++ frontend/src/Components/Assests/data.js
@@ -0,0 +1,38 @@
+import Coconut from '../Assests/coconut.png'
+import Turmeric from '../Assests/turmeric.png'
+import Rice from '../Assests/rice.png'
+import Guva from '../Assests/Guava.png'
+
+let data_product = [
+    {
+        id:1,
+        name:"Coconut",
+        image:Coconut,
+        new_price:35,
+        old_price:50.
+    },
+    {
+        id:2,
+        name:"Guva",
+        image:Guva,
+        new_price:20,
+        old_price:30.
+    },
+    {
+        id:3,
+        name:"Turmeric",
+        image:Turmeric,
+        new_price:150,
+        old_price:200.
+    },
+    {
+        id:4,
+        name:"Rice",
+        image:Rice,
+        new_price:75,
+        old_price:95.
+    },
+]
+
+
+export default data_product
---
+++ frontend/src/Components/NewProducts/NewProducts.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './NewProducts.css'
+
+
+const NewProducts=()=>{
+    return(
+        <div className="new-collection">
+            <h1>NEW PRODUCTS</h1>
+            <hr />
+            <div className="products">
+                
+            </div>
+
+        </div>
+    )
+}
+
+export default NewProducts
