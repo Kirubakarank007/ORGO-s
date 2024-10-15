b2e3a8307ea11e833c16e441d1907376e3478108
KIRUBAKARAN K <kirubakaran02002@gmail.com>
2024-10-15 21:08:44 +0530

    Navbar & routes Added

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import './Navbar.css'
+import logo from '../Assests/plant.png'
+import cart_icon from '../Assests/cart-icon.png'
+import { Link } from "react-router-dom";
+
+
+const Navbar=()=>{
+
+    const[menu,setMenu]=useState("shop")
+
+    return(
+        <div className="navbar">
+            <div className="nav-logo">
+                <img src={logo} alt="" />
+                <p>ORGO's</p>
+            </div>
+            <ul className="nav-menu">
+                <li onClick={()=>setMenu("shop")} > <Link to='/' style={{textDecoration:'none'}}>Shop</Link> {menu==="shop"?<hr/>:<></>} </li>
+                <li onClick={()=>setMenu("fruits")}> <Link to='/fruits' style={{textDecoration:'none'}}>fruits</Link>{menu==="fruits"?<hr/>:<></>}</li>
+                <li onClick={()=>setMenu("seeds")}><Link to='/seeds' style={{textDecoration:'none'}}>Seeds</Link>{menu==="seeds"?<hr/>:<></>}</li>
+                <li onClick={()=>setMenu("more")}><Link to='/more' style={{textDecoration:'none'}}>more</Link>{menu==="more"?<hr/>:<></>}</li>
+            </ul>
+            <div className="nav-login-cart">
+                <Link to='/login'><button>Login</button></Link> 
+                <Link to='/cart'> <img src={cart_icon} alt="" /> </Link>
+                <div className="nav-cart-count">0</div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
---
+++ frontend/src/Pages/Shop.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Hero from "../Components/Hero/Hero";
+import Popular from "../Components/Popular/Popular";
+import Offer from "../Components/Offer/Offer";
+
+const Shop = ()=>{
+    return (
+        <div>
+            <Hero/>
+            <Popular/>
+            <Offer/>
+
+        </div>
+    )
+}
+
+export default Shop
